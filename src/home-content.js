4d3420ae747100810646b40716d0964188ee2419
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-09 15:37:08 -0700

    Change restaurant theme

--- src/home-content.js
+++ src/home-content.js
@@ -1,4 +1,4 @@
-import homeImg from "./assets/home.jpg";
+import homeImg from "./assets/berry-tart.jpg";
 
 function generateHome(doc) {
   const image = doc.createElement("img");
@@ -10,8 +10,8 @@ function generateHome(doc) {
     doc.createElement("p"),
     doc.createElement("p"),
   );
-  greeting.querySelector("h1").textContent = "Vietnamese Food World";
-  greeting.querySelector("p:nth-of-type(1)").textContent = "Lots of authentic Vietnamese food";
+  greeting.querySelector("h1").textContent = "Julia's Strawberry Cafe";
+  greeting.querySelector("p:nth-of-type(1)").textContent = "Berries galore!";
   greeting.querySelector("p:nth-of-type(2)").textContent = "Click the navigational links above to find what you're looking for.";
   
   const content = doc.querySelector("#content");
