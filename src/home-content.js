3dc32348b309e4bb83df6bbaa3e6a41e68bfdfe8
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-12 14:15:32 -0700

    Adjust homepage content & styling
    It is appropriate to have the address and phone number on the home page.

--- src/home-content.js
+++ src/home-content.js
@@ -3,13 +3,13 @@ import homeImg from "./assets/berry-tart.jpg";
 
 const DAY_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const DAY_HOURS = {
-  0: "7:00am - 9:00pm",
+  0: "10:00am - 9:00pm",
   1: "7:00am - 9:00pm",
   2: "7:00am - 9:00pm",
   3: "7:00am - 9:00pm",
   4: "7:00am - 9:00pm",
   5: "7:00am - 9:00pm",
-  6: "7:00am - 9:00pm",
+  6: "10:00am - 9:00pm",
 };
 
 /**
@@ -51,9 +51,8 @@ function generateHome(doc) {
   quoteCard.appendChild(
     (() => {
       const blockQuote = doc.createElement("blockquote");
-      const p1 = doc.createElement("p");
+      const [p1, p2] = [doc.createElement("p"), doc.createElement("p")];
       p1.textContent = "“I'm never coming here again!”";
-      const p2 = doc.createElement("p");
       p2.textContent = "— A very satisfied customer";
       blockQuote.append(p1, p2);
       return blockQuote;
@@ -63,16 +62,20 @@ function generateHome(doc) {
   const hoursCard = createCard(doc);
   hoursCard.append(
     ...(() => {
-      const h2 = doc.createElement("h2");
-      h2.textContent = "Hours";
+      const h2first = doc.createElement("h2");
+      h2first.textContent = "Hours";
       const ul = doc.createElement("ul");
-      // No, I can't use a for-loop like a normal human being
       ul.append(...Array.from({length: 7}, (ele, idx) => {
         const li = doc.createElement("li");
         li.textContent = `${DAY_NAMES[idx]}: ${DAY_HOURS[idx]}`;
         return li;
       }));
-      return [h2, ul];
+      const h2second = doc.createElement("h2");
+      h2second.textContent = "Location";
+      const address = doc.createElement("address");
+      address.textContent = "🏠 123 Street St., Town Center, WA 92123\n📞 (123) 456-7890";
+
+      return [h2first, ul, h2second, address];
     })()
   );
 
