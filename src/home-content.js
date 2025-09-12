0f78cf3aae300210dd7bce72b48a4cfe3304b46d
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-11 22:07:33 -0700

    Change homepage to be generated via JS (spec #7)
    As such, the hard-coded content was removed from template.html.

--- src/home-content.js
+++ src/home-content.js
@@ -1,11 +1,82 @@
+import createCard from "./card-generator.js";
 import homeImg from "./assets/berry-tart.jpg";
 
+const DAY_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const DAY_HOURS = {
+  0: "7:00am - 9:00pm",
+  1: "7:00am - 9:00pm",
+  2: "7:00am - 9:00pm",
+  3: "7:00am - 9:00pm",
+  4: "7:00am - 9:00pm",
+  5: "7:00am - 9:00pm",
+  6: "7:00am - 9:00pm",
+};
+
+/**
+ * 
+ * @param {HTMLDocument} doc 
+ */
 function generateHome(doc) {
   const content = doc.querySelector("#content");
-  const image = doc.createElement("img");
-  image.src = homeImg;
 
-  content.textContent = "home";
+  const titleCard = createCard(doc, "title-card");
+  titleCard.appendChild(
+    (() => {
+      const h1 = doc.createElement("h1");
+      h1.textContent = "Julia's Strawberry Cafe";
+      return h1;
+    })()
+  );
+  
+  const imgCard = createCard(doc);
+  imgCard.appendChild(
+    (() => {
+      const img = doc.createElement("img");
+      img.src = homeImg;
+      img.alt = "A tart layered with cream, sliced strawberries, and a blueberry in the center";
+      return img;
+    })()
+  );
+
+  const aboutCard = createCard(doc);
+  aboutCard.appendChild(
+    (() => {
+      const p = doc.createElement("p");
+      p.textContent = "With handpicked strawberries sourced from our local farms, we prepare the best strawberry desserts in town! To view our menu or contact information, please click the above links.";
+      return p;
+    })()
+  );
+
+  const quoteCard = createCard(doc);
+  quoteCard.appendChild(
+    (() => {
+      const blockQuote = doc.createElement("blockquote");
+      const p1 = doc.createElement("p");
+      p1.textContent = "“I'm never coming here again!”";
+      const p2 = doc.createElement("p");
+      p2.textContent = "— A very satisfied customer";
+      blockQuote.append(p1, p2);
+      return blockQuote;
+    })()
+  );
+
+  const hoursCard = createCard(doc);
+  hoursCard.append(
+    ...(() => {
+      const h2 = doc.createElement("h2");
+      h2.textContent = "Hours";
+      const ul = doc.createElement("ul");
+      // No, I can't use a for-loop like a normal human being
+      ul.append(...Array.from({length: 7}, (ele, idx) => {
+        const li = doc.createElement("li");
+        li.textContent = `${DAY_NAMES[idx]}: ${DAY_HOURS[idx]}`;
+        return li;
+      }));
+      return [h2, ul];
+    })()
+  );
+
+  content.append(titleCard, imgCard, aboutCard, quoteCard, hoursCard);
 }
 
 export default generateHome;
