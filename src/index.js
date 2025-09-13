97f8f80d487d2fb8ae0cbb6772079aa702f5e4fc
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-13 10:11:58 -0700

    Refactor wipeContent to not use innerHTML

--- src/index.js
+++ src/index.js
@@ -24,10 +24,11 @@ const displayControl = (function (doc) {
     });
   });
 
-  // @TODO maybe use a more semantic way to clear it? by removing the nodes
   function wipeContent() {
     const content = doc.querySelector("#content");
-    content.innerHTML = "";
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
+    }
   }
 })(document);
 
