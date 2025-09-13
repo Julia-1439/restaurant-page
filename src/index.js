a6cf4211423324df20b5b64bd6f0bcc1ab7d46c5
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-13 10:16:00 -0700

    Refactor: remove the unnecessary IIFE

--- src/index.js
+++ src/index.js
@@ -4,31 +4,30 @@ import generateHome from "./home-content.js";
 import generateMenu from "./menu-content.js";
 import generateContact from "./contact-content.js";
 
-// @note: iife module might not be needed here, but good to not have much global code I guess!
-const displayControl = (function (doc) {
-  generateHome(doc);
-
-  const nav = doc.querySelector("nav");
-  const navButtons = nav.querySelectorAll("button");
-  navButtons.forEach((btn) => {
-    btn.addEventListener("click", (evt) => {
-      wipeContent();
-      switch (evt.target.id) {
-        case "home": generateHome(doc);
-          break;
-        case "menu": generateMenu(doc);
-          break;
-        case "contact": generateContact(doc);
-          break;
-      }
-    });
-  });
-
-  function wipeContent() {
-    const content = doc.querySelector("#content");
-    while (content.firstChild) {
-      content.removeChild(content.firstChild);
+const doc = document;
+generateHome(doc);
+const nav = doc.querySelector("nav");
+const navButtons = nav.querySelectorAll("button");
+navButtons.forEach((btn) => {
+  btn.addEventListener("click", (evt) => {
+    wipeContent();
+    switch (evt.target.id) {
+      case "home": generateHome(doc);
+      break;
+      case "menu": generateMenu(doc);
+      break;
+      case "contact": generateContact(doc);
+      break;
     }
+  });
+});
+
+
+
+function wipeContent() {
+  const content = doc.querySelector("#content");
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
   }
-})(document);
+}
 
